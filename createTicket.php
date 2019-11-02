6730f5a0632fd49a43e816cef7ebe14259abb2ad
Luis G Luna <luis.lunapa@outlook.com>
2019-11-02 12:12:30 -0600

    Create ticket service

--- createTicket.php
+++ createTicket.php
@@ -1,3 +1,20 @@
 <?php
 require_once('header.php');
 $data = json_validate(file_get_contents('php://input'));
+
+foreach($data as $invitee) {
+    $name = $invitee['name'];
+    $arrived = $invitee['arrived'];
+    $rawRepresentation = md5(uniqid(rand(), true));
+
+    $insertQuery = $db ->queryInsert(
+        "Inserts the invitees to the db",
+        array ("
+            INSER INTO 
+                Ticket
+                (name, arrived, rawRepresentaion)
+                VALUES
+                ('$name', $arrived, '$rawRepresentation')
+        ")
+    );
+}
