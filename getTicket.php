5268ebdd8f2fede1d7cd284422ee46d6256ef087
Luis G Luna <luis.lunapa@outlook.com>
2019-11-02 12:01:52 -0600

    Deleted photo from query

--- getTicket.php
+++ getTicket.php
@@ -61,7 +61,6 @@ if (isset($_GET['id']) && trim($_GET['id']) != "") {
         "SELECT
         t.id,
         t.name,
-        t.photo,
         t.arrived
         FROM Ticket t
         WHERE t.name LIKE '%$nameTicket%'
@@ -72,7 +71,6 @@ if (isset($_GET['id']) && trim($_GET['id']) != "") {
         array_push($response, array(
                 'id' => $row['id'],
                 'name' => $row['name'],
-                'photo' => $row['photo'],
                 'arrived' => $row['arrived']
             )
         );
@@ -84,7 +82,6 @@ if (isset($_GET['id']) && trim($_GET['id']) != "") {
         "SELECT
         t.id,
         t.name,
-        t.photo,
         t.arrived
         FROM Ticket t
 
@@ -95,7 +92,6 @@ if (isset($_GET['id']) && trim($_GET['id']) != "") {
         array_push($response, array(
                 'id' => $row['id'],
                 'name' => $row['name'],
-                'photo' => $row['photo'],
                 'arrived' => $row['arrived']
             )
         );
@@ -103,112 +99,3 @@ if (isset($_GET['id']) && trim($_GET['id']) != "") {
 }
 echo(json_encode($response));
 exit;
-//if (!isset($_GET['id']) || trim($_GET['id']) == "") {
-//    /// Get all buses
-//    $allBuses = $db->querySelect(
-//        "Get all routes",
-//        "SELECT
-//        b.Id,
-//        b.name,
-//        b.color
-//    FROM
-//        BusRoutes b
-//    "
-//    );
-//
-//
-//    while ($row = $allBuses->fetch_assoc()) {
-//
-//        $id = $row['Id'];
-//        $points = $db->querySelect(
-//            "Get points for every route",
-//            "
-//        SELECT
-//            *
-//        FROM
-//        RoutePoints r
-//        WHERE
-//        r.route = $id
-//        "
-//        );
-//
-//
-//        $coordinates = array();
-//
-//        while ($coordRow = $points->fetch_assoc()) {
-//            array_push($coordinates, array(
-//                'latitude' => $coordRow['latitude'],
-//                'longitude' => $coordRow['longitude']
-//            ));
-//        }
-//
-//        array_push($response, array(
-//
-//                'id' => $row['Id'],
-//                'name' => $row['name'],
-//                'color' => $row['color'],
-//                'points' => $coordinates
-//
-//            )
-//        );
-//    }
-//
-//} else {
-//    /// Get stops for the given route id
-//    $idRoute = $_GET['id'];
-//
-//    $allBuses = $db->querySelect(
-//        "Get the stops for the given route",
-//        "SELECT
-//        b.Id,
-//        b.latitude,
-//        b.longitude,
-//        b.name,
-//        b.photo
-//    FROM
-//        BusStops b INNER JOIN
-//        BusSchedule s ON
-//        b.Id = s.stop
-//
-//    WHERE
-//        s.route = $idRoute
-//    "
-//    );
-////$db->printQuery();
-//
-//
-//    while ($row = $allBuses->fetch_assoc()) {
-//        array_push($response, array(
-//
-//                'id' => $row['Id'],
-//                'latitude' => $row['latitude'],
-//                'longitude' => $row['longitude'],
-//                'name' => $row['name'],
-//                'photo' => $row['photo']
-//            )
-//        );
-//    }
-//
-//
-//}
-//
-//$staticModels = array(
-//    array(
-//        'id' => 1,
-//        'route' => 1,
-//        'serialNumber' => 'DS3',
-//        'plates' => 'GYU4321'
-//    ),
-//    array(
-//        'id' => 3,
-//        'route' => 5,
-//        'serialNumber' => 'GD24T',
-//        'plates' => 'SEFWS3'
-//    )
-//
-//
-//);
-//
-//
-//echo(json_encode($response));
-//
