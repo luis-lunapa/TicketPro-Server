2dab175e246f3e04ceb891698d5caa0984215cf7
Luis G Luna <luis.lunapa@outlook.com>
2019-11-06 17:54:07 -0600

    Tests to send ticket

--- sendTicket.php
+++ sendTicket.php
@@ -14,56 +14,57 @@ $response = json_decode($raw);
 print_r($response);
 
 foreach($response as $invitee) {
-
-    $id = $invitee['id'];
-    $name = $invitee['name'];
-    $arrived = $invitee['arrived'];
-    $email = $invitee['email'];
-    $ticketSent = $invitee['ticketSent'];
-
-
-    if (!$ticketSent) {
-
-        $generateTicketAPI = "generateTicket.php?id=" . $id;
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $generateTicketAPI
-        ));
-
-        curl_exec($curl);
-
-
-        $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-        $email_to = "jesus.cruz@oracle.com";
-        $email_subject = "Oracle Party Ticket";
-
-        $message = "<html><head>
-<title>Your email at the time</title>
-</head>
-<body>
-<img src=\"/generatedTicket/'$idTicket'.jpg\">
-</body>";
-
-$success = mail($email_to, $email_subject , $message,$headers);
-
-        echo 'Successfully sent == ' . $success;
-        if (!$success) {
-            $errorMessage = error_get_last()['message'];
-            echo $errorMessage;
-            header("HTTP/1.1 500");
-        } else {
-            echo 'Successfully sent';
-        }
-
-
-
-    }
-
-
-
-
-
+    print_r($invitee);
 }
+//    $id = $invitee['id'];
+//    $name = $invitee['name'];
+//    $arrived = $invitee['arrived'];
+//    $email = $invitee['email'];
+//    $ticketSent = $invitee['ticketSent'];
+//
+//
+//    if (!$ticketSent) {
+//
+//        $generateTicketAPI = "generateTicket.php?id=" . $id;
+//        $curl = curl_init();
+//        curl_setopt_array($curl, array(
+//            CURLOPT_RETURNTRANSFER => 1,
+//            CURLOPT_URL => $generateTicketAPI
+//        ));
+//
+//        curl_exec($curl);
+//
+//
+//        $headers  = 'MIME-Version: 1.0' . "\r\n";
+//        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+//        $email_to = "jesus.cruz@oracle.com";
+//        $email_subject = "Oracle Party Ticket";
+//
+//        $message = "<html><head>
+//<title>Your email at the time</title>
+//</head>
+//<body>
+//<img src=\"/generatedTicket/'$idTicket'.jpg\">
+//</body>";
+//
+//$success = mail($email_to, $email_subject , $message,$headers);
+//
+//        echo 'Successfully sent == ' . $success;
+//        if (!$success) {
+//            $errorMessage = error_get_last()['message'];
+//            echo $errorMessage;
+//            header("HTTP/1.1 500");
+//        } else {
+//            echo 'Successfully sent';
+//        }
+//
+//
+//
+//    }
+//
+//
+//
+//
+//
+//}
 header("HTTP/1.1 200 OK");
