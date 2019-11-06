f8ccbfeec9abaf180798e4f1b74512dfd6445ac7
Luis G Luna <luis.lunapa@outlook.com>
2019-11-05 18:27:14 -0600

    Consulting api for image

--- generateTicket.php
+++ generateTicket.php
@@ -8,6 +8,14 @@ if (!isset($_GET['id']) && trim($_GET['id']) == "") {
 $idTicket = $_GET['id'];
 
 
-$ticketGen = "https://barcode.tec-it.com/barcode.ashx?data=" . "id:$idTicket" . "&code=PDF417&multiplebarcodes=false&translate-esc=false&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&qunit=Mm&quiet=0";
+$ticketAPI = "https://barcode.tec-it.com/barcode.ashx?data=" . "id:$idTicket" . "&code=PDF417&multiplebarcodes=false&translate-esc=false&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&qunit=Mm&quiet=0";
 
-echo $ticketGen;
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $ticketAPI
+));
+
+$response = curl_exec($curl);
+
+echo $response;
