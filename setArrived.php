b179cdd30c859b8aea2e1032bb0b7c4bbebf459f
Luis G Luna <luis.lunapa@outlook.com>
2019-11-07 19:50:45 -0600

    Changed field to read only

--- setArrived.php
+++ setArrived.php
@@ -2,6 +2,11 @@
 include_once ('header.php');
 
 header('Content-Type: application/json');
+if (isset($_GET['debug'])) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 $response = array();
 
 $id = "";
