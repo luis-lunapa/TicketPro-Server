e3014301a3b758a810cfc4c244a0604ee759d9ca
Luis G Luna <luis.lunapa@outlook.com>
2019-11-16 00:35:19 -0600

    Changed position

--- generateTicket.php
+++ generateTicket.php
@@ -18,7 +18,7 @@ $ticketImage = imagecreatefrompng('resources/ticket.png');
 imagealphablending($ticketImage, false);
 imagesavealpha($ticketImage, true);
 
-$done = imagecopymerge($ticketImage, $ticketCode, 140, 759, 0, 0, 250, 40, 100);
+$done = imagecopymerge($ticketImage, $ticketCode, 130, 730, 0, 0, 250, 40, 100);
 
 if ($done) {
 
