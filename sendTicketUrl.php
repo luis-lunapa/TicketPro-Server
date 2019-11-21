826c2fc5e15c61227f378550cd8b14632e867440
Luis G Luna <luis.lunapa@outlook.com>
2019-11-21 10:37:34 -0600

    Send tickets url service

---
+++ sendTicketUrl.php
@@ -0,0 +1,23 @@
+<?php
+require_once('header.php');
+
+if (isset($_GET['debug'])) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+$allTicketsAPI = $domain . "tickets/getTicket.php";
+
+$ch = curl_init ($allTicketsAPI);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+
+$raw=curl_exec($ch);
+
+$response = json_decode($raw, true);
+foreach($response as $invitee) {
+    $id = $invitee['id'];
+    $url = $domain . "tickets/ticket.php?id=$id";
+    echo $url;
+}
