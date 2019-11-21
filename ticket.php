1670249a9d845db7b8a0934bd35c430596d090e5
Luis G Luna <luis.lunapa@outlook.com>
2019-11-21 09:48:12 -0600

    Ticket image service

---
+++ ticket.php
@@ -0,0 +1,12 @@
+<?php
+$idTicket = "";
+if (!isset($_GET['id']) && trim($_GET['id']) == "") {
+    header("HTTP/1.1 422 Unprocessable Entity");
+    exit;
+}
+$idTicket = $_GET['id'];
+
+$ticketImg = imagecreatefrompng("generatedTickets/TO" . $idTicket . ".png");
+
+header('Content-Type: image/png');
+imagepng($ticketImg);
