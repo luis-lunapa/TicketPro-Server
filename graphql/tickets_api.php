7cc8a1c3f4fe021bdadb3490a405413aa8eac1bc
Luis G Luna <luis.lunapa@outlook.com>
2019-12-09 13:06:45 -0600

    First version of graphql api

---
+++ graphql/tickets_api.php
@@ -0,0 +1,20 @@
+<?php
+require "graphql-php/autoload.php";
+require "../header.php";
+require "DB.php";
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQl\Type\Definition\Type;
+use GraphQL\Type\Schema;
+use GraphQL\GraphQL;
+
+
+
+
+
+
+
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
