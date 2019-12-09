a548ccf74924a4f23abd5616b59a5ced41aae2a3
Luis G Luna <luis.lunapa@outlook.com>
2019-12-09 13:21:58 -0600

    GraphQl sample execution

--- graphql/tickets_api.php
+++ graphql/tickets_api.php
@@ -14,13 +14,37 @@ use GraphQl\Type\Definition\Type;
 use GraphQL\Type\Schema;
 use GraphQL\GraphQL;
 
+try {
+    // Object Definitions
 
 
+    $schema = new Schema([
+        'query' => 'queryType'
+    ]);
 
+    $rawinput = file_get_contents('php://input');
+    $input = json_decode($rawinput);
 
+    $query = $input["query"];
+
+    $variableValues = isset($input['variables']) ? $input['variables'] : null;
+
+    $result = GraphQL::executeQuery($schema, $query, null, null, $variableValues);
+
+    $output = $result->toArray();
+
+} catch (\Exception $e) {
+    $output = [
+        'error' => [
+            'message' => $e->getMessage()
+        ]
+    ];
+}
 
 
 header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
+echo json_encode($output);
